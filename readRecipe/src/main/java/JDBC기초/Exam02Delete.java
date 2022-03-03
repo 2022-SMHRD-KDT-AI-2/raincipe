73c2db2cc5bb84bca0eda9dd5b0d4473353558f3
smhrd <smhrd@221.156.60.57>
2022-03-03 16:19:09 +0900

    로그인

---
+++ readRecipe/src/main/java/JDBC기초/Exam02Delete.java
@@ -0,0 +1,78 @@
+package JDBC기초;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Exam02Delete {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+	
+		Connection conn = null;
+		PreparedStatement psmt = null;
+		
+		// 1. 사용자로부터 ID와 PW 입력받기
+		
+		System.out.print("ID 입력 : ");
+		String id = scan.next();
+		
+		System.out.print("PW 입력 : ");
+		String pw = scan.next();
+		
+		// 2. 아이디와 비밀번호가 일치할때만 회원탈퇴시키기(JDBC)
+			try {
+					Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+		// 2-1. DB연결
+				String url = "jdbc:oracle:thin:@localhost:1521:xe";
+				String user = "hr";
+				String userPw = "hr";
+		
+				conn = DriverManager.getConnection(url, user, userPw);
+				
+		
+		// 2-2. SQL문 전송
+				String sql = "delete aimemeber where id = ? and pw = ?"; // 다해놓고 못함 ? 쓰는법 확인
+				
+				psmt = conn.prepareStatement(sql);
+				
+				psmt.setNString(1, id);
+				psmt.setNString(2, pw);
+				
+				int r = psmt.executeUpdate();
+			
+							
+		// 3. 회원탈퇴 성공시 회원탈퇴 성공! 출력
+				 if(r > 0) {
+						System.out.println("회원탈퇴 성공!");
+					}else {
+		// 4. 회원탈퇴 실패시 회원탈퇴 실패.. 출력
+						System.out.println(" 회원탈퇴 실패..");
+					}
+			} catch(ClassNotFoundException e) {
+				e.printStackTrace();
+				System.out.println("클래스 찾지못함");
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("SQL관련문제");
+			}finally {
+		
+				// 5. 닫기
+				try {
+					if(psmt != null) {
+						psmt.close();
+					}
+					if(conn != null) {
+						conn.close();
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+					System.out.println("자원반납시 오류");
+				}
+			}
+	}
+}
